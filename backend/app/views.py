4b4be2b45fa0b23721a67527341b2a91bfa74dae
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-05-11 23:16:38 +0530

    Category & Transaction api added

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from django.db.models.functions import TruncMonth
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category, Transaction
+from .serializers import CategorySerializer, TransactionSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.select_related('category').all()
+    serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        type_filter = self.request.query_params.get('type')
+        category = self.request.query_params.get('category')
+        month = self.request.query_params.get('month')
+        search = self.request.query_params.get('search')
+        if type_filter:
+            qs = qs.filter(type=type_filter)
+        if category:
+            qs = qs.filter(category_id=category)
+        if month:
+            year, m = month.split('-')
+            qs = qs.filter(date__year=year, date__month=m)
+        if search:
+            qs = qs.filter(title__icontains=search)
+        return qs
+
