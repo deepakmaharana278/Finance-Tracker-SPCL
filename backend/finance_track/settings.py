1e8feedbfb985910d551e20371be1405e71a9683
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-05-14 20:35:40 +0530

    For Backend Deploy

--- backend/finance_track/settings.py
+++ backend/finance_track/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-9$be7$t7zx$drp&n1axp*ypujm7pxwqaea55-yqx10551pjvui
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
